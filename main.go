98df304543131ac5bc89c04e89a4d6a7cfa85115
lei2025-zip <lei1770271561@163.com>
2020-12-20 18:59:18 +0800

    2020年12月20日18:59:14

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	_ "BitcoinWeb/routers"
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+    fmt.Println("team-A's Project 2020年12月20日18:37:55")
+
+	beego.SetStaticPath("/js","./static/js")
+	beego.SetStaticPath("/img","./static/img")
+	beego.SetStaticPath("/css","./static/css")
+
 	beego.Run()
 }
 
