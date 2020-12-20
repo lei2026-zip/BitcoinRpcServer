ce23428160748b331f6a917fbc63265857c20280
lei2025-zip <lei1770271561@163.com>
2020-12-20 12:53:59 +0800

    sd

--- routers/router.go
+++ routers/router.go
@@ -6,6 +6,10 @@ import (
 )
 
 func init() {
+<<<<<<< HEAD
     beego.Router("/come_in", &controllers.MainController{})
 
+=======
+    beego.Router("/", &controllers.MainController{})
+>>>>>>> 63255a460d513fd8129d6894126702f578a33058
 }
