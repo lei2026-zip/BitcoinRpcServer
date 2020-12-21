2c3d1d03af8049434a7a48403393a3657dad61b1
1457980874 <1457980874@qq.com>
2020-12-21 10:14:23 +0800

    fix jspost

--- static/js/network.js
+++ static/js/network.js
@@ -1,37 +1,33 @@
-var xmlobj; //定义XMLHttpRequest对象
-function CreateXMLHttpRequest()
-{
-    if(window.ActiveXObject)
-//如果当前浏览器支持Active Xobject，则创建ActiveXObject对象
-    {
-        //xmlobj = new ActiveXObject("Microsoft.XMLHTTP");
-        try {
-            xmlobj = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                xmlobj = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (E) {
-                xmlobj = false;
-            }
-        }
+function httpPost(URL, PARAMS) {
+    var temp = document.createElement("form");
+    temp.action = URL;
+    temp.method = "post";
+    temp.style.display = "none";
+
+    for (var x in PARAMS) {
+        var opt = document.createElement("textarea");
+        opt.name = x;
+        opt.value = PARAMS[x];
+        temp.appendChild(opt);
     }
-    else if(window.XMLHttpRequest)
-//如果当前浏览器支持XMLHttp Request，则创建XMLHttpRequest对象
-    {
-        xmlobj = new XMLHttpRequest();
-    }
+
+    document.body.appendChild(temp);
+    temp.submit();
+
+    return temp;
 }
-function SubmitArticle(act,cityname,antique) //主程序函数
-{
-    CreateXMLHttpRequest(); //创建对象
-    //var parm = "act=firstweather" ;//构造URL参数
-    //antique = escape(antique);
-    var parm = "act=" + act + "&cityname=" + cityname + "&antique=" + antique;//构造URL参数
-    //xmlobj.open("POST", "{dede:global.cfg_templeturl/}/../include/weather.php", true); //调用weather.php
-    xmlobj.open("POST", "/weather/include/weather.php", true); //调用weather.php
-    xmlobj.setRequestHeader("cache-control","no-cache");
-    xmlobj.setRequestHeader("contentType","text/html;charset=uft-8") //指定发送的编码
-    xmlobj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");  //设置请求头信息
-    xmlobj.onreadystatechange = StatHandler;  //判断URL调用的状态值并处理
-    xmlobj.send(parm); //设置为发送给服务器数据
+
+function toMonitorCompanyList(groupName,riskLevel,updateStatus,province,eventLevel,reportId,riskStatus){
+
+    var params = {
+        "groupName": encodeURIComponent(groupName),
+        "riskLevel": riskLevel,
+        "updateStatus": updateStatus,
+        "province": encodeURIComponent(province),
+        "eventLevel": eventLevel,
+        "reportId": reportId,
+        "riskStatus":riskStatus
+    };
+
+    httpPost(ctx + "/js/network", params);
 }
