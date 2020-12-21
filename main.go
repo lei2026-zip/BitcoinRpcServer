a29fb28f78870b91baedb019d8aec9ea39405ee7
1457980874 <1457980874@qq.com>
2020-12-21 09:47:11 +0800

    ReadCommandFile

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"BitcoinWeb/controllers/ReadFile"
 	_ "BitcoinWeb/routers"
 	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	fmt.Println(string(ReadFile.CommandFileReader()))
     fmt.Println("team-A's Project 2020年12月20日18:37:55")
 
 	beego.SetStaticPath("/js","./static/js")
@@ -14,5 +16,6 @@ func main() {
 	beego.SetStaticPath("/css","./static/css")
 
 	beego.Run()
+
 }
 
