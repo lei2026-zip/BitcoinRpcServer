02a0846a0c4eae6bc325a87fe97a41ede5f10ce1
xiaoyuziZCY <1162735365@qq.com>
2020-12-21 19:23:29 +0800

    2020-12-21--xiaoyuzi

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	_ "BitcoinWeb/routers"
+	"BitcoinRpcServer/db_mysql"
+	_ "BitcoinRpcServer/routers"
 	"fmt"
 	"github.com/astaxie/beego"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
     fmt.Println("team-A's Project 2020年12月20日18:37:55")
 
+	db_mysql.ConnectDB()
 	beego.SetStaticPath("/js","./static/js")
 	beego.SetStaticPath("/img","./static/img")
 	beego.SetStaticPath("/css","./static/css")
--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,17 @@
 package routers
 
 import (
-	"BitcoinWeb/controllers"
+	"BitcoinRpcServer/controllers"
 	"github.com/astaxie/beego"
 )
 
 
 func init() {
-    beego.Router("/Bitcoin", &controllers.RpcController{})
+	//先执行“/”打开注册页面
+	beego.Router("/", &controllers.MainController{})
+	//执行“register”打开登录页面
+	beego.Router("/register", &controllers.RegisterController{})
+	//执行“login”打开主页面
+	beego.Router("/login",&controllers.LoginingController{})
+	beego.Router("/Bitcoin", &controllers.RpcController{})
 }
