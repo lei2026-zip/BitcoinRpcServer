67ca010b3426f2a52ffe97ccf544d681cda094b3
lei2025-zip <lei1770271561@163.com>
2020-12-22 08:32:11 +0800

    2020-12-22 08:32:09

--- main.go
+++ main.go
@@ -10,9 +10,14 @@ func main() {
     fmt.Println("team-A's Project 2020年12月20日18:37:55-")
 
 //	db_mysql.ConnectDB()
-	beego.SetStaticPath("/js","./static/js")
+    //分包处理
+	beego.SetStaticPath("/js","./static/js/tool/")
+	beego.SetStaticPath("/jsc","./static/js/")
+
 	beego.SetStaticPath("/img","./static/img")
-	beego.SetStaticPath("/css","./static/css")
+
+	beego.SetStaticPath("/CSS","./static/css")
+	beego.SetStaticPath("/css","./static/css/Entity")
 
 	beego.Run()
 }
